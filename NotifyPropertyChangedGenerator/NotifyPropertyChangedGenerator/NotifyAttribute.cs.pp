6b69da61b8da70f071e61865cff01384b558e63c
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-06-04 21:53:09 +0900

    Make attributes conditional in NotifyAttribute.cs.pp
    These are never used at runtime.

--- NotifyPropertyChangedGenerator/NotifyPropertyChangedGenerator/NotifyAttribute.cs.pp
+++ NotifyPropertyChangedGenerator/NotifyPropertyChangedGenerator/NotifyAttribute.cs.pp
@@ -1,15 +1,18 @@
 ﻿// used from NotifyPropertyChangedGenerator
 
 using System;
+using System.Diagnostics;
 
 namespace $rootnamespace$
 {
+    [Conditional("NEVER_USED_AT_RUNTIME")]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
     internal sealed class NotifyAttribute : Attribute
     {
 
     }
 
+    [Conditional("NEVER_USED_AT_RUNTIME")]
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
     internal sealed class NonNotifyAttribute : Attribute
     {
